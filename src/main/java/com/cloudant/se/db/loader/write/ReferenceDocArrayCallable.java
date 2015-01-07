c07050baf663729b9c5527145aecd5156521db83
cavanaugh-ibm <joey@cloudant.com>
2015-01-07 13:36:09 -0500

    Fixing a bad checkin

--- src/main/java/com/cloudant/se/db/loader/write/ReferenceDocArrayCallable.java
+++ src/main/java/com/cloudant/se/db/loader/write/ReferenceDocArrayCallable.java
@@ -1,43 +1,39 @@
 package com.cloudant.se.db.loader.write;
 
+import java.io.IOException;
 import java.util.Map;
+
+import org.apache.log4j.Logger;
 
 import com.cloudant.se.db.loader.config.AppConfig;
 import com.cloudant.se.db.loader.config.DataTable;
+import com.cloudant.se.db.loader.exception.StructureException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.collect.Lists;
 
-public class ReferenceObjectArrayCallable extends ABaseObjectCallable {
-	public ReferenceObjectArrayCallable(AppConfig config, DataTable table) {
+public class ReferenceDocArrayCallable extends BaseDocCallable {
+	protected static final Logger	log	= Logger.getLogger(NestedDocArrayCallable.class);
+
+	public ReferenceDocArrayCallable(AppConfig config, DataTable table) {
 		super(config, table);
 	}
 
 	@Override
 	public Integer handle() throws Exception {
-		try {
+		//
+		// This case is a little more complex -
+		// - We need to insert the base object (upsert)
+		// - We need to add a reference to the parent object (upsert)
+		//
 
-			//
-			// This case is a little more complex -
-			// - We need to insert the base object (upsert)
-			// - We need to add a reference to the parent object (upsert)
-			//
-
-			if (!upsert(id, toMap())) {
-				throw new Exception("Unable to upsert a document after seceral attempts.  I should handle this error better - [parentId=" + parentId + "][id=" + id + "]");
-			}
-
-			if (!upsert(parentId, buildEmptyParent(Lists.newArrayList(REF_PREFIX + id)))) {
-				throw new Exception("Unable to upsert a document after seceral attempts.  I should handle this error better - [parentId=" + parentId + "][id=" + id + "]");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		upsert(id, toMap());
+		upsert(parentId, buildEmptyParent(Lists.newArrayList(REF_PREFIX + id)));
 
 		return 0;
 	}
 
 	@Override
-	protected Map<String, Object> handleConflict() throws Exception {
+	protected Map<String, Object> handleConflict() throws StructureException, JsonProcessingException, IOException {
 		Map<String, Object> fromCloudant = getFromCloudant(parentId);
 		addToArrayAt(fromCloudant, table.nestField, REF_PREFIX + id);
 
--- src/main/java/com/cloudant/se/db/loader/write/ReferenceDocCallable.java
+++ src/main/java/com/cloudant/se/db/loader/write/ReferenceDocCallable.java
@@ -1,44 +1,38 @@
 package com.cloudant.se.db.loader.write;
 
+import java.io.IOException;
 import java.util.Map;
+
+import org.apache.log4j.Logger;
 
 import com.cloudant.se.db.loader.config.AppConfig;
 import com.cloudant.se.db.loader.config.DataTable;
+import com.cloudant.se.db.loader.exception.StructureException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
-public class ReferenceObjectCallable extends ABaseObjectCallable {
-	public ReferenceObjectCallable(AppConfig config, DataTable table) {
+public class ReferenceDocCallable extends BaseDocCallable {
+	protected static final Logger	log	= Logger.getLogger(NestedDocArrayCallable.class);
+
+	public ReferenceDocCallable(AppConfig config, DataTable table) {
 		super(config, table);
 	}
 
 	@Override
 	public Integer handle() throws Exception {
-		try {
+		//
+		// This case is a little more complex -
+		// - We need to insert the base object (upsert)
+		// - We need to add a reference to the parent object (upsert)
+		//
 
-			//
-			// This case is a little more complex -
-			// - We need to insert the base object (upsert)
-			// - We need to add a reference to the parent object (upsert)
-			//
-
-			Map<String, Object> parent = buildEmptyParent(REF_PREFIX + id);
-			Map<String, Object> map = toMap();
-
-			if (!upsert(id, map)) {
-				throw new Exception("Unable to upsert a document after seceral attempts.  I should handle this error better - [parentId=" + parentId + "][id=" + id + "]");
-			}
-
-			if (!upsert(parentId, parent)) {
-				throw new Exception("Unable to upsert a document after seceral attempts.  I should handle this error better - [parentId=" + parentId + "][id=" + id + "]");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		upsert(id, toMap());
+		upsert(parentId, buildEmptyParent(REF_PREFIX + id));
 
 		return 0;
 	}
 
 	@Override
-	protected Map<String, Object> handleConflict() throws Exception {
+	protected Map<String, Object> handleConflict() throws StructureException, JsonProcessingException, IOException {
 		Map<String, Object> fromCloudant = getFromCloudant(parentId);
 		fromCloudant.put(table.nestField, REF_PREFIX + id);
 
